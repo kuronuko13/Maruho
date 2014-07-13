356d9435b1123d117f08cb409a36671e6269df20
Hiroaki Yamashita <genesis.kagura@gmail.com>
2014-07-14 07:48:46 +0900

    topの作成

--- page-top.php
+++ page-top.php
@@ -7,126 +7,67 @@ get_header(); ?>
 <div id="container" class="innerBox">
 	<!-- [ #content ] -->
 	<div id="content">
-
-	<?php if ( have_posts()) : the_post(); ?>
-
-	<?php if (get_post_type() === 'page') : ?>
+	<a href="<?php echo home_url('/'); ?>/list/"><img src="<?php echo get_template_directory_uri(); ?>/images/site-img/toppage-img/h_news.png" alt="お知らせ"></a>
+	<a href="<?php echo home_url('/'); ?>/list/"><img src="<?php echo get_template_directory_uri(); ?>/images/site-img/toppage-img/btn_ichiran.png" alt="一覧"></a><br/>
+	<?php query_posts('cat=6&post_type=post&showposts=4'); ?>
 	<?php
-	$topFreeContent = NULL;
-	$topFreeContent = get_the_content();
-	if ($topFreeContent) : ?>
-	<div id="topFreeArea">
-	<?php if (bizVektorOptions('topEntryTitleDisplay') == true) : ?>
-		<h2><?php the_title(); ?></h2>
-	<?php endif; // bizVektorOptions('topEntryTitleDisplay') == true ?>
-		<?php the_content(); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . 'Pages:', 'after' => '</div>' ) ); ?>
-	</div>
-	<?php endif; // $topFreeContent ?>
-	<?php endif; // get_post_type() === 'page' ?>
-
-	<?php if ( is_user_logged_in() == TRUE ) {
-	global $user_level;
-	get_currentuserinfo(); ?>
-		<div class="adminEdit">
-			<?php if (10 <= $user_level) { ?>
-			<p class="caption">
-			<?php _e('* In the page [Settings] &raquo; [Display Settings], if the setting of the front page is not [page], nothing is displayed in this area.', 'biz-vektor'); ?><br />
-			<?php _e('* If empty, the body field of a page that you set on the front page does not display anything.', 'biz-vektor'); ?><br />
-			<?php // _e('* If you have set a fixed page to the front page, paging does not appear in the bottom of the post the list at the bottom.', 'biz-vektor'); ?>
-			</p>
-			<?php } ?>
-			<span class="linkBtn linkBtnS linkBtnAdmin" style="float:left;margin-right:10px;"><?php edit_post_link( __('Edit', 'biz-vektor') ); ?></span>
-			<?php if (10 <= $user_level) { ?>
-			<span style="float:left;margin-right:10px;"><a href="<?php echo site_url(); ?>/wp-admin/themes.php?page=theme_options#topPage" class="btn btnS btnAdmin">
-				<?php _e('Title display setting', 'biz-vektor'); ?>
-			</a></span>
-			<span><a href="<?php echo site_url(); ?>/wp-admin/options-reading.php" class="btn btnS btnAdmin">
-				<?php _e('Change display page', 'biz-vektor'); ?>
-			</a></span>
-			<?php } ?>
-		</div>
-	<?php } // login ?>
-
-<?php endif; // have_posts() ?>
-
-<?php get_template_part('module_topPR'); ?>
-
-<?php if ( function_exists( 'biz_vektor_topSpecial' ) ): biz_vektor_topSpecial(); endif; ?>
-
-<?php
-/*-------------------------------------------*/
-/*	info
-/*-------------------------------------------*/
-?>
-<?php $loop = new WP_Query( array( 'post_type' => 'info', 'posts_per_page' => 5, ) ); ?>
-<?php if ($loop->have_posts()) : ?>
-<div id="topInfo" class="infoList">
-<h2><?php echo esc_html(bizVektorOptions('infoLabelName')); ?></h2>
-<div class="rssBtn"><a href="<?php echo home_url(); ?>/feed/?post_type=info" id="infoRss" target="_blank">RSS</a></div>
-<?php
-$options = biz_vektor_get_theme_options();
-if ( $options['listInfoTop'] == 'listType_set' ) { ?>
-	<?php while ( $loop->have_posts() ) : $loop->the_post();?>
-		<?php get_template_part('module_loop_post2'); ?>
-	<?php endwhile ?>
-<?php } else { ?>
-	<ul class="entryList">
-	<?php while ( $loop->have_posts() ) : $loop->the_post();?>
-		<?php get_template_part('module_loop_post'); ?>
-	<?php endwhile; ?>
-	</ul>
-<?php } ?>
-</div><!-- [ /#topInfo ] -->
-<?php endif;?>
-
-<?php // wp_reset_query();?>
-
-<?php
-/*-------------------------------------------*/
-/*	Post
-/*-------------------------------------------*/
-?>
-<?php
-$postTopCount = bizVektorOptions('postTopCount');
-$postTopCount = mb_convert_kana($postTopCount, "a", "UTF-8");
-if ($postTopCount != '0') : ?>
-<?php
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-$post_loop = new WP_Query( array(
-	'post_type' => 'post',
-	'posts_per_page' => $postTopCount,
-	'paged' => $paged
-) ); ?>
-<?php if ($post_loop->have_posts()): ?>
-	<div id="topBlog" class="infoList">
-	<h2><?php echo esc_html(bizVektorOptions('postLabelName')); ?></h2>
-	<div class="rssBtn"><a href="<?php echo home_url(); ?>/feed/?post_type=post" id="blogRss" target="_blank">RSS</a></div>
-	<?php $options = biz_vektor_get_theme_options();
-	if ( $options['listBlogTop'] == 'listType_set' ) { ?>
-		<?php while ( $post_loop->have_posts() ) : $post_loop->the_post();?>
-			<?php get_template_part('module_loop_post2'); ?>
-		<?php endwhile ?>
-	<?php } else { ?>
-		<ul class="entryList">
-		<?php while ( $post_loop->have_posts() ) : $post_loop->the_post();?>
-			<?php get_template_part('module_loop_post'); ?>
+	if (have_posts()) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<article>
+				<li>
+					<time pubdate="pubdate" datetime="<?php the_time('Y-n-d-'); ?>">
+					<?php the_time('Y年'); ?><br/>
+					<?php the_time('n月d日'); ?>
+					</time>
+					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+				</li>
+			</article>
 		<?php endwhile; ?>
-		</ul>
-	<?php } ?>
-	<?php // pagination($post_loop->max_num_pages); ?>
-	</div><!-- [ /#topBlog ] -->
-<?php endif; // $post_loop have_posts() ?>
-<?php endif; // $postTopCpunt= 0 ?>
-<?php wp_reset_query();?>
+	<?php endif;  ?>
+	<?php wp_reset_query(); ?>
 
-<?php biz_vektor_blogList() // RSS import ?>
+	<a href="<?php echo home_url('/'); ?>/shopinfo/"><img src="<?php echo get_template_directory_uri(); ?>/images/site-img/toppage-img/h_shopinfo.png" alt="店舗紹介"></a><br/>
+	<a href="<?php echo home_url('/'); ?>/shopinfo/"><img src="<?php echo get_template_directory_uri(); ?>/images/site-img/toppage-img/img_shopinfo.png" alt="店舗紹介画像"></a><br/>
+	常時70種類以上のクラフトビールとこだわりの地酒を取り揃えております。<br/>
 
-<?php biz_vektor_topContentsBottom(); ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/images/site-img/toppage-img/h_calender.png" alt="営業日カレンダー"><br/>
 
-<?php biz_vektor_fbLikeBoxFront(); ?>
-<?php biz_vektor_snsBtns(); ?>
-<?php biz_vektor_fbComments(); ?>
+	<a href="<?php echo home_url('/'); ?>/list/"><img src="<?php echo get_template_directory_uri(); ?>/images/site-img/toppage-img/h_event.png" alt="イベント"></a>
+	<a href="<?php echo home_url('/'); ?>/list/"><img src="<?php echo get_template_directory_uri(); ?>/images/site-img/toppage-img/btn_ichiran.png" alt="一覧"></a><br/>
+	<?php query_posts('cat=7&post_type=post&showposts=4'); ?>
+	<?php
+	if (have_posts()) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<article>
+				<li>
+					<time pubdate="pubdate" datetime="<?php the_time('Y-n-d-'); ?>">
+					<?php the_time('Y年'); ?><br/>
+					<?php the_time('n月d日'); ?>
+					</time>
+					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+				</li>
+			</article>
+		<?php endwhile; ?>
+	<?php endif;  ?>
+	<?php wp_reset_query(); ?>
+
+	<a href="<?php echo home_url('/'); ?>/mind/"><img src="<?php echo get_template_directory_uri(); ?>/images/site-img/toppage-img/h_mind.png" alt="想い"></a><br/>
+	<a href="<?php echo home_url('/'); ?>/mind/"><img src="<?php echo get_template_directory_uri(); ?>/images/site-img/toppage-img/img_mind.png" alt="想い画像"></a><br/>
+	今の時代の酒屋として、変わるべき<br/>
+	もの変わらずに持ち続けるべき理念<br/>
+	について<br/>
+
+	<a href="<?php echo home_url('/'); ?>/blog/"><img src="<?php echo get_template_directory_uri(); ?>/images/site-img/toppage-img/h_blog.png" alt="マルホ便り"></a><br/>
+	<a href="<?php echo home_url('/'); ?>/blog/"><img src="<?php echo get_template_directory_uri(); ?>/images/site-img/toppage-img/img_blog.png" alt="マルホ便り画像"></a><br/>
+	ブルワリーや蔵元訪問、日々のあれ<br/>
+	これについて書いています。<br/>
+
+	<a href="https://www.facebook.com/maruho.sake"><img src="<?php echo get_template_directory_uri(); ?>/images/site-img/toppage-img/h_blog.png" alt="Facebook"></a><br/>
+	<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fmaruho.sake&amp;width=218&amp;height=290&amp;colorscheme=light&amp;show_faces=true&amp;header=true&amp;stream=false&amp;show_border=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:218px; height:290px;" allowTransparency="true"></iframe><br/
+
+	<a href="<?php echo home_url('/'); ?>/project/"><img src="<?php echo get_template_directory_uri(); ?>/images/site-img/toppage-img/h_project.png" alt="プロジェクト"></a><br/>
+	<a href="https://beerbox.jp/"><img src="<?php echo get_template_directory_uri(); ?>/images/site-img/toppage-img/img_beerbox.png" alt="beerbox"></a><br/>
+	クラフトビール定期宅配サービス
 
 	</div>
 	<!-- [ /#content ] -->
