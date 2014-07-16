046afcbaaaadbd6c8124fc41b7f0ab304ee9b430
Hiroaki Yamashita <genesis.kagura@gmail.com>
2014-07-17 01:05:45 +0900

    last

---
+++ page-commingsoon.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * The main template file.
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Style-Type" content="text/css">
+<link href="<?php echo get_template_directory_uri(); ?>/comingsoon.css" rel="stylesheet" type"text/css"  />
+<title>マルホ酒店</title></head>
+<body>
+<hr >
+<img src="<?php echo get_template_directory_uri(); ?>/images/toplogo.png" alt="マルホ酒店">
+<img src="<?php echo get_template_directory_uri(); ?>/images/omote.jpg" alt="試飲会" align="right"  >
+<div id="cont">
+	只今HP移行中となっております。<br/>
+	7月17日頃オープン予定ですので、ご迷惑をお掛け致しますがもう暫くお待ちください。<br/>
+	<h3>お問い合わせ先<br/></h3>
+	<table>
+	<tr>
+		<td>マルホ酒店</td>
+		<td>営業時間</td>
+	</tr>
+	<tr>
+		<td>Tel:06-6583-2368  </td>
+		<td>平日9:00〜20:00</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>土曜日9:00〜17:00</td>
+	</tr>
+	</table>
+</div>
+<br clar="right">
+<hr >
+</body>
